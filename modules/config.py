fdde05c6bae05922113f466a2582411ea66d52ee
sanjaykagra86 <sanjaykagra86@gmail.com>
2024-12-27 15:19:00 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
-# config.py
-BOT_NAME = "Free bot"
-BOT_TOKEN = "8040014845:AAHAo3zgpikp-4j2PLQofMxSwtoOdQKTQLg"
-API_ID = 28328736
-API_HASH = "802254a44896baa87f3083b7af36b2e5"
+import os
+
+API_ID    = os.environ.get("API_ID", "24495656")
+API_HASH  = os.environ.get("API_HASH", "61afcf68c6429714dd18acd07f246571")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8040014845:AAHAo3zgpikp-4j2PLQofMxSwtoOdQKTQLg") 
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
