dd76cf1035a89fc08df4e0994831148d529e9d28
sanjaykagra86 <sanjaykagra86@gmail.com>
2024-12-27 15:58:23 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,5 +1,5 @@
 import os
-
+BOT_NAME = "Moni Uploader 2"
 API_ID    = os.environ.get("API_ID", "24495656")
 API_HASH  = os.environ.get("API_HASH", "61afcf68c6429714dd18acd07f246571")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8040014845:AAHAo3zgpikp-4j2PLQofMxSwtoOdQKTQLg") 
