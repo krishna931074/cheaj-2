3ca9c122f122df2784717c95fff0ea8769a4568a
krishna931074 <kumarrohit92572@gmail.com>
2024-12-31 16:58:41 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,5 +1,5 @@
-BOT_NAME = "Moni Uploader 2"
-API_ID    = 24495656
-API_HASH  = "61afcf68c6429714dd18acd07f246571"
-BOT_TOKEN = "8040014845:AAHAo3zgpikp-4j2PLQofMxSwtoOdQKTQLg"
+BOT_NAME = "Stranger bot"
+API_ID    = 21567814
+API_HASH  = "cd7dc5431d449fd795683c550d7bfb7e"
+BOT_TOKEN = "6783049418:AAGOJ6ZOVyeXiBsF5jG3i-01S7wtyageckw"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
