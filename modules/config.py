2cedade57a1d9f987a7295c9b66926935d84ee9e
STRANGER BOY 💔 BROKEN 👦 <kumarrohit92572@gmail.com>
2025-03-09 16:49:01 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,5 +1,5 @@
 BOT_NAME = "Stranger bot"
 API_ID    = 21567814
 API_HASH  = "cd7dc5431d449fd795683c550d7bfb7e"
-BOT_TOKEN = "6783049418:AAGOJ6ZOVyeXiBsF5jG3i-01S7wtyageckw"
+BOT_TOKEN = "7058856750:AAEClHhNLDdNgX517vnux3AR83fWvaizNiI"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
